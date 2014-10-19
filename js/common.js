165ad06320f2f12a0131d03773a34a0401e71a39
fanian <rfanian@gmail.com>
2014-10-19 14:35:37 +0400

    add footer and done small fixes

--- js/common.js
+++ js/common.js
@@ -3,13 +3,13 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
-//   $(window).scroll(function () {
-//       if ( $(this).scrollTop() > 200) {
-//           $('.scrollMenu').slideDown();
-//       } else if ( $(this).scrollTop() <= 200 ) {
-//           $('.scrollMenu').slideUp();
-//       }
-//   });
+   $(window).scroll(function () {
+       if ( $(this).scrollTop() > 200) {
+           $('.scrollMenu').slideDown();
+       } else if ( $(this).scrollTop() <= 200 ) {
+           $('.scrollMenu').slideUp();
+       }
+   });
 
 //    jQuery(document).ready(function($) {
 //        var nav = $('.menu');
@@ -36,6 +36,7 @@ head.ready(function() {
     $('#form3').validate();
     $('#form4').validate();
     $('#form5').validate();
+    $('#form6').validate();
 
     $('.fancybox').fancybox({
         helpers:{
